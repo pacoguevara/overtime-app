3f3434a448076ad6914e5bdfd3576f864f5949ca
Paco Guevara <pacoguevaraa@gmail.com>
2020-10-13 23:38:18 -0700

    Implemented edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: :show
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
 
-
   def new
     @post = Post.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      redirect_to :edit
+    end
   end
 
   def create
